84906cf92918590969c76fec50d572172db72b3f
Ruman Ahmed Shaikh <s.k.ruman.0@gmail.com>
2021-12-26 21:59:27 +0530

    Card design selected

--- src/Card.js
+++ src/Card.js
@@ -8,23 +8,15 @@ class ProjectCard extends Component {
     return (
       <div className="tc">
         <h1>Hello {this.props.name}</h1>
-        <Button variant="primary">Primary</Button>{' '}
-        <Button variant="secondary">Secondary</Button>{' '}
-        <Button variant="success">Success</Button>{' '}
-        <Button variant="warning">Warning</Button>{' '}
-        <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
-        <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
-        <Button variant="link">Link</Button>
         <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
+            <Card.Body>
+                <Card.Subtitle style={{ fontSize: '20px', textEmphasisColor: 'black' ,textEmphasisStyle: 'bold' }}>Card Title</Card.Subtitle>
+                <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+                </Card.Text>
+                <Card.Link href="#">GitHub</Card.Link>
+            </Card.Body>
         </Card>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import ProjectCard from './Card';
+import MultiActionAreaCard from './Cards2'
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<ProjectCard />, document.getElementById('root'));
+ReactDOM.render(<MultiActionAreaCard />, document.getElementById('root'));
 
 reportWebVitals();
